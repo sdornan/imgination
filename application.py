4149e512ed9d064f4ee6ea882d172ef8998cd4a6
Sam Dornan <sdornan@gmail.com>
2012-09-13 15:49:53 -0700

    You can upload images now

--- application.py
+++ application.py
@@ -1,17 +1,23 @@
-from flask import Flask, request, flash, redirect, url_for, render_template
+from flask import Flask, abort, request, flash, redirect, url_for, render_template, send_from_directory
 
 from flask.ext.uploads import UploadSet, IMAGES, configure_uploads
 from flask.ext.wtf import Form, FileField, file_allowed, file_required
 
-from werkzeug import secure_filename
+from datetime import datetime
+import os
+import redis
+from uuid import uuid4
+
+REDIS_PREFIX = 'image:'
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ewoipjfisoafsdpo'
+app.config['UPLOADS_DEFAULT_DEST'] = UPLOAD_FOLDER
 
 
-images = UploadSet('images', IMAGES, default_dest=lambda app: '/uploads/')
+images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
-
 
 
 class UploadForm(Form):
@@ -24,17 +30,35 @@ class UploadForm(Form):
 def home():
     form = UploadForm()
     if form.validate_on_submit():
-        filename = secure_filename(form.upload.data.filename)
+        filename = images.save(form.upload.data)
+        image_id = unicode(uuid4())[:8]
+        r = redis.StrictRedis(host='localhost')
+        image = {
+            'filename': filename,
+            'uploaded_by': request.remote_addr,
+            'uploaded_at': datetime.now()
+        }
+        r.hmset(REDIS_PREFIX + image_id, image)
         flash("Success")
-    else:
-        filename = None
-    return render_template('home.html', form=form, filename=filename)
+        return redirect(url_for('show', id=image_id))
+
+    return render_template('home.html', form=form)
 
 
-# @app.route('/images/<id>')
-# def show(id):
-#     image = 
+@app.route('/<id>')
+def show(id):
+    r = redis.StrictRedis(host='localhost')
+    image = r.hgetall(REDIS_PREFIX + id)
+    if not image:
+        abort(404)
+    url = images.url(image['filename'])
+    return render_template('show.html', url=url, image=image)
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == '__main__':
